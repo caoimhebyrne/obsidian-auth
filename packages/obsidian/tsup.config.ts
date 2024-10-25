2fbed5351d0f847c908fbb5d0b9de2e735216d09
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2024-10-25 16:28:04 +0100

    refactor: add a shared config for typescript

--- packages/obsidian/tsup.config.ts
+++ packages/obsidian/tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	format: ["esm"],
 	entry: ["src/index.ts"],
+	format: ["esm"],
 	splitting: true,
 	treeshake: true,
 	dts: true,
